5143a8b3fe0bffe96abbe664fd0bab3663ac0fb2
ARYAN SRIVASTAVA <103809985+ap9140@users.noreply.github.com>
2024-07-17 19:12:09 +0530

    heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import { BsChatLeftHeartFill } from "react-icons/bs";
 
 export default function Header() {
   return (
-    <div className=''>
-      <div className=''>
+    <div className='shadow-sm border-b sticky top-0 bg-blue-50 z-30 p-3'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto'>
         {/* Your content for the header goes here */}
         <Link href={"/"} className=''>
-            <h1 className='text-red-500 text-2xl font-bold p-2 font-serif'>VoxAura</h1>
+        <h1 className='text-2xl font-bold p-2 font-serif' style={{ color: '#102C57' }}>VoxAura</h1>
         </Link>
         <Link href={"/"}>
             <BsChatLeftHeartFill />
